ba79b19f6bd732ce09a09213e7aaba29543fe87b
sheldon <452081365@qq.com>
2021-01-26 00:20:22 +0800

    提交

--- src/main/java/com/test/tree/rbTree/RbTree.java
+++ src/main/java/com/test/tree/rbTree/RbTree.java
@@ -1,7 +1,5 @@
 package com.test.tree.rbTree;
 
-import java.util.Comparator;
-
 /**
  * 红黑树实现
  *
@@ -51,7 +49,7 @@ public class RbTree<T extends Comparable<T>> {
      * @param tree
      * @param node
      */
-    public void rightRotaion(RbTree<T> tree, RbTreeNode<T> node) {
+    public void rightRotation(RbTree<T> tree, RbTreeNode<T> node) {
         RbTreeNode<T> left = node.getLeft();
         node.setLeft(left.getRight());
         if (left.getRight() != tree.nil) {
@@ -95,11 +93,11 @@ public class RbTree<T extends Comparable<T>> {
     /**
      * 总结插入修复最多不会超过3次修复动作，就能让红黑树恢复性质
      * 其中插入新节点之后，当前节点默认就是红色，如果父节点也是红色，那么就需要进行修正，因为违反了性质4，
-     * 首先要找到当前节点的叔叔节点
-     * 第一步判断叔叔节点的颜色，如果叔叔节点是红色的，那么就将父节点和叔叔节点都染成黑色的，将爷爷节点染成红色的，然后指针移动到爷爷节点
-     * 第二部，如果叔叔节点是黑色的，那么判断当前节点的方向是否与叔叔节点方向一样，比如叔叔节点是爷爷节点的右孩子，当前节点也是父节点的右孩子，
+     * 首先要找到当前节点的叔叔节点 ,然后根据叔叔节点的颜色和当前节点的方向与叔叔节点方向的关系进行不同的处理
+     * 第一种情况，如果叔叔节点是红色的，那么就将父节点和叔叔节点都染成黑色的，将爷爷节点染成红色的，然后指针移动到爷爷节点
+     * 第二种情况，如果叔叔节点是黑色的，那么判断当前节点的方向是否与叔叔节点方向一样，比如叔叔节点是爷爷节点的右孩子，当前节点也是父节点的右孩子，
      * 那么就需要将指针移动到父节点，然后执行一个与当前节点方向相反方向的旋转，本例中为左旋
-     * 第三步，如果叔叔节点是黑色的，那么判断当前节点的方向是否与叔叔节点方向相反，例如叔叔节点是爷爷节点的右孩子，当前节点是父节点的左孩子，
+     * 第三种情况，如果叔叔节点是黑色的，那么判断当前节点的方向是否与叔叔节点方向相反，例如叔叔节点是爷爷节点的右孩子，当前节点是父节点的左孩子，
      * 那么需要将父节点染成黑色的，将爷爷节点染成红色的，然后将指针移动到爷爷节点，最后对爷爷节点执行一个与当前节点相反方向的旋转，本例中为右旋
      * @param tree
      * @param node
@@ -131,7 +129,7 @@ public class RbTree<T extends Comparable<T>> {
                     node.getParent().setColor(1);
                     node.getParent().getParent().setColor(0);
                     node = node.getParent().getParent();
-                    rightRotaion(tree,node);
+                    rightRotation(tree,node);
                 }
                 //如果当前节点的父节点是爷爷节点的右孩子
             }else{
@@ -146,7 +144,7 @@ public class RbTree<T extends Comparable<T>> {
                     //如果叔叔节点是黑色的，并且当前节点是父节点的左孩子，那么将指针移到父节点，然后对父节点进行右旋
                 }else if(node == node.getParent().getLeft()){
                     node = node.getParent();
-                    rightRotaion(tree,node);
+                    rightRotation(tree,node);
                     //如果叔叔节点是黑色的，并且当前节点是父节点的右孩子，那么将父节点设置为黑色的，将爷爷节点变成红色的，
                     //最后对爷爷节点进行左旋
                 }else{
@@ -191,6 +189,9 @@ public class RbTree<T extends Comparable<T>> {
     }
 
     private void delete(RbTree<T> tree, RbTreeNode<T> node){
+        /**
+         * 用tmp记录当前要删除或者要移动到树中的那个
+         */
         RbTreeNode<T> tmp = node;
         int originColor = tmp.getColor();
         RbTreeNode<T> child = null;
@@ -216,26 +217,39 @@ public class RbTree<T extends Comparable<T>> {
             tmp.getLeft().setParent(tmp);
             tmp.setColor(node.getColor());
         }
-        if(originColor == 0){
-
+        if(originColor == 1){
+            deleteFix(tree,child);
         }
     }
 
+    /**
+     * 总结一下，当删除一个节点之后可以通过染黑和旋转恢复红黑树的性质，步骤不会超过4次
+     * 需要判断当前节点与兄弟节点的颜色关系，以及兄弟节点的孩子节点的颜色和位置关系来做不同的处理
+     * 第一种情况，如果兄弟节点是红色的，那么将兄弟节点设置成黑色的，将父节点设置成红色的，并且对父节点执行一次与当前节点位置相同方向的旋转
+     * 第二种情况，如果兄弟节点是黑色的，并且两个孩子节点也都是黑色的，那么将兄弟节点设置成黑色的，并且将指针移动到当前节点的父节点，进行下一轮处理
+     * 第三种情况，如果兄弟节点是黑色的，并且兄弟节点中那个与自己位置相反方向的孩子节点颜色是黑色的，但是另外一个孩子的颜色是红色的，
+     * 那么就交换兄弟节点和另外一个孩子节点的颜色，将兄弟节点设置成黑色的，将另外一个孩子设置成红色的，并且对兄弟节点执行一次与自己位置相反方向的旋转
+     * 第四种情况，如果兄弟节点是黑色的，但是兄弟节点中那个与自己位置相反方向的孩子节点颜色是红色的，那么需要交换兄弟节点与父节点的颜色，并且将与自己位置
+     * 相反方向的那个孩子节点的颜色设置成黑色的，并且对父节点执行一次与自己位置相同方向的旋转，将指针指向树的根节点，推出循环
+     * 最后将根节点设置成黑色的
+     * @param tree
+     * @param node
+     */
     private void deleteFix(RbTree<T> tree, RbTreeNode<T> node){
         //当当前节点不是根节点，并且当前节点的颜色为黑色时继续执行调整
-        while(node != tree.nil && node.getColor() ==  0){
+        while(node != tree.nil && node.getColor() ==  1){
             //如果当前节点是父节点的左孩子
             if(node == node.getParent().getLeft()){
                 //那么获取到当前节点的兄弟节点，也就是当前节点父节点的有孩子
                 RbTreeNode<T> brother = node.getParent().getRight();
                 //如果右兄弟节点是红色的
-                if(brother.getColor() == 1){
+                if(brother.getColor() == 0){
                     /**
                      * 兄弟节点设置成黑色的
                      * 父节点设置成红色的
                      */
-                    brother.setColor(0);
-                    node.getParent().setColor(1);
+                    brother.setColor(1);
+                    node.getParent().setColor(0);
                     /**
                      * 做与兄弟节点位置相反方向的选转，当前兄弟节点是右，那么就对父节点做左旋转，这样兄弟节点这个黑色节点就变成了当前子树的根节点
                      */
@@ -246,18 +260,17 @@ public class RbTree<T extends Comparable<T>> {
                 /**
                  * 当兄弟节点的右孩子是黑色的，并且兄弟节点的左孩子也是黑色的时候，那么将兄弟节点染红，然后指针移动到父节点继续执行
                  */
-                if(brother.getRight().getColor() == 0 && brother.getLeft().getColor() == 0){
-                    brother.setColor(1);
+                if(brother.getRight().getColor() == 1 && brother.getLeft().getColor() == 1){
+                    brother.setColor(0);
                     node = node.getParent();
                     /**
                      * 如果兄弟节点是黑色的，并且兄弟节点的右孩子是黑色，但是左孩子是红色的时候，将左孩子染黑，兄弟节点设置成红色的，
                      * 将兄弟节点按照染黑孩子的相反方向选转，最后获取节点新的兄弟节点
                      */
-                }else if(brother.getRight().getColor() == 0 && brother.getLeft().getColor() == 1){
-                    brother.getLeft().setColor(0);
-                    brother.setColor(1);
-                    rightRotaion(tree,brother);
-                    brother = node.getParent().getRight();
+                }else if(brother.getRight().getColor() == 1 && brother.getLeft().getColor() == 0){
+                    brother.getLeft().setColor(1);
+                    brother.setColor(0);
+                    rightRotation(tree,brother);
                 }else{
                     /**
                      * 如果当前兄弟节点是黑色的，但是兄弟节点的右孩子是红色的，那么不管兄弟节点的左孩子是什么颜色
@@ -268,38 +281,60 @@ public class RbTree<T extends Comparable<T>> {
                      * 将当前指针指向树的根节点，退出循环
                      */
                     brother.setColor(node.getParent().getColor());
-                    node.getParent().setColor(0);
-                    brother.getRight().setColor(0);
+                    node.getParent().setColor(1);
+                    brother.getRight().setColor(1);
                     leftRotation(tree,node.getParent());
                     node = tree.root;
                 }
             }else{
+                /**
+                 * 如果当前节点是父节点的右孩子，那么当前节点的兄弟节点就是父节点的左孩子
+                 */
                 RbTreeNode<T> brother = node.getParent().getLeft();
-                if(brother.getColor() == 1){
-                    brother.setColor(0);
-                    node.getParent().setColor(1);
-                    rightRotaion(tree,node.getParent());
+                /**
+                 * 如果兄弟节点是红色的，那么将兄弟节点染黑，父节点染红，然后对父节点进行一次与兄弟节点位置相反方向的旋转
+                 * 如果兄弟节点是左孩子，那么就进行右旋
+                 * 然后获取到新的兄弟节点，并且新的兄弟节点一定是黑色的
+                 */
+                if(brother.getColor() == 0){
+                    brother.setColor(1);
+                    node.getParent().setColor(0);
+                    rightRotation(tree,node.getParent());
                     brother = node.getParent().getLeft();
                 }
-                if(brother.getLeft().getColor() == 0&& brother.getRight().getColor() == 0){
-                    brother.setColor(1);
+                /**
+                 * 如果兄弟节点是黑色的，并且兄弟节点的左孩子是黑色的，并且右孩子也是黑色的
+                 * 那么兄弟节点设置成红色，并且将指针向上移动到父节点
+                 */
+                if(brother.getLeft().getColor() == 1&& brother.getRight().getColor() == 1){
+                    brother.setColor(0);
                     node = node.getParent();
-                }else if(brother.getLeft().getColor() == 0){
-                    brother.getRight().setColor(0);
-                    brother.setColor(1);
+                    /**
+                     * 如果兄弟节点是黑色的，并且兄弟节点的左孩子是黑色的，并且兄弟节点右孩子是红色的，
+                     * 那么交换兄弟节点和兄弟节点右孩子的颜色
+                     * 并且对兄弟节点致性一次与兄弟节点位置相反方向的旋转，这里是进行一次左旋
+                     */
+                }else if(brother.getLeft().getColor() == 1&&brother.getRight().getColor() ==0){
+                    brother.getRight().setColor(1);
+                    brother.setColor(0);
                     leftRotation(tree, brother);
-                    brother = node.getParent().getLeft();
+                    /**
+                     * 如果兄弟节点是红色的，并且兄弟节点的左孩子也是红色的，那么交换兄弟节点和父节点的颜色
+                     * 并且将兄弟节点的右孩子染黑
+                     * 然后对父节点执行一次与兄弟节点位置相反方向的旋转，这里是进行一次右旋
+                     * 最后将当前指针指向树的根节点，让其在下一次执行中推出循环
+                     */
                 }else{
                     brother.setColor(node.getParent().getColor());
-                    node.getParent().setColor(0);
-                    node.getLeft().setColor(0);
-                    rightRotaion(tree,node.getParent());
+                    node.getParent().setColor(1);
+                    node.getLeft().setColor(1);
+                    rightRotation(tree,node.getParent());
                     node = tree.root;
                 }
             }
         }
         //最后将根节点染黑
-        node.setColor(0);
+        node.setColor(1);
     }
 
     private RbTreeNode<T> getMinimun(RbTree<T> tree, RbTreeNode<T> node){
