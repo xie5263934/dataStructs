8cabd599fe6fd63539bb0e68ae5a52b879edf5a4
sheldon <452081365@qq.com>
2021-02-09 00:25:21 +0800

    字符串匹配BM算法

--- src/test/java/com/test/stringmatch/StringMatchTest.java
+++ src/test/java/com/test/stringmatch/StringMatchTest.java
@@ -50,4 +50,26 @@ public class StringMatchTest {
         RKMatch rkMatch = new RKMatch(main);
         System.out.println(rkMatch.indexOf(pattern));
     }
+
+
+    @Test
+    public void testBM() {
+        char[] main = new char[10];
+        main[0] = 'a';
+        main[1] = 'c';
+        main[2] = 'd';
+        main[3] = 'y';
+        main[4] = 'e';
+        main[5] = 'x';
+        main[6] = 'm';
+        main[7] = 'a';
+        main[8] = 'b';
+        main[9] = 'c';
+        char[] pattern = new char[3];
+        pattern[0] = 'm';
+        pattern[1] = 'a';
+        pattern[2] = 'b';
+        BMMatch bmMatch = new BMMatch();
+        System.out.println(bmMatch.indexOf(main,pattern));
+    }
 }
