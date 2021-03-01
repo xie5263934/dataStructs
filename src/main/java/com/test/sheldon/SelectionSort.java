cdd1d520180f899b3d2b554c1459f0328fc721b9
jinrun.xie <jinrun.xie@dmall.com>
2021-03-01 14:13:21 +0800

    修改选择排序法bug

--- src/main/java/com/test/sheldon/SelectionSort.java
+++ src/main/java/com/test/sheldon/SelectionSort.java
@@ -2,18 +2,20 @@ package com.test.sheldon;
 
 public class SelectionSort {
 
-    public void sort(int arr []){
-        if(arr.length <= 1){
+    public void sort(int arr[]) {
+        if (arr.length <= 1) {
             return;
         }
-        for(int i = 0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             int min = arr[i];
-            for(int j = i+1; j<arr.length; j++){
-                if(min > arr[j]){
+            for (int j = i + 1; j < arr.length; j++) {
+                if (min > arr[j]) {
+                    int tmp = min;
                     min = arr[j];
+                    arr[j] = tmp;
                 }
             }
-            arr[i]=min;
+            arr[i] = min;
         }
     }
 }
